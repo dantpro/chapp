6d314d2cf68bee5cc2e1d3adb1f688acf40cbbbb
Dmitry Antonov <dant@dant.pro>
2021-11-16 16:08:36 +0700

    initial
    initial

---
+++ choco-app-install-work.cmd
@@ -0,0 +1,68 @@
+@echo off
+
+::Set-ExecutionPolicy Unrestricted
+::iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+::choco feature enable -n allowGlobalConfirmation
+
+
+
+choco install 7zip
+choco install azure-data-studio
+
+choco install bulkrenameutility
+
+choco install choco-protocol-support
+choco install chocolateygui
+choco install conemu
+choco install far
+choco install firefox
+choco install googlechrome
+
+choco install kindle
+
+choco install irfanview
+choco install irfanviewplugins
+choco install irfanview-languages
+
+::choco install linqpad
+
+choco install javaruntime
+choco install openjdk11
+::choco install AdoptOpenJDK11
+
+choco install putty
+
+choco install mremoteng
+choco install sharex
+
+choco install sql-server-management-studio
+
+choco install sumatrapdf
+
+#choco install telegram
+
+choco install vscode
+
+choco install winmerge
+
+choco install openssl.light
+
+
+choco install sysinternals
+
+::choco install nirlauncher /sysinternals
+
+choco install git
+choco install github-desktop
+choco install tortoisegit
+
+#choco install powershell-core
+
+choco install notepadplusplus
+
+choco install golang
+choco install python3
+
+
+
+
---
+++ choco-app-upd.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+::choco upgrade chocolatey
+
+choco upgrade all
+
+
